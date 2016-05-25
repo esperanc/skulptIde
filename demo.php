ad62db96a467acf57d46e3ccd7b07214721f45fc
Claudio Esperança <claudio.esperanca@gmail.com>
2016-05-25 18:22:21 -0300

    Added the demo.php utility and updated the skulpt library

---
+++ demo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<h1> Some Skulpt/Processing demo programs </h1>
+<h3> Click one of the following </h3>
+<?php
+$dir    = './demoSketches';
+echo "<ul>";
+foreach (scandir($dir) as $i => $filename) {
+    if ($i>=2) {
+        echo "<li> <a href=index.html?autorun&program=$dir/$filename> $filename </a></li>";
+    }
+}
+echo "</ul>";
+?>
+</body>
+</html>
